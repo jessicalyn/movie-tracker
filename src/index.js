0b84b9624c02772622a69dc2898c598558b07688
Niraj Aryal <niroz.aryal11@gmail.com>
2019-03-19 16:07:24 -0600

    Adds redux
    Co-authored-by: Jessica Hansen <jessicalynhansen@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './components/App/App';
+import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
+import { rootReducer } from './Reducers';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-const router = (
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  
+  )
+
+  ReactDOM.render(
+    <Provider store={store}>
     <BrowserRouter>
         <App/>
     </BrowserRouter>
-)
-ReactDOM.render(router, document.getElementById('root'));
+    </Provider> , 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
