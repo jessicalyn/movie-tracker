e2f3ef3ee742511e268921dbd3ec23b127be6c4a
Niraj Aryal <niroz.aryal11@gmail.com>
2019-03-23 18:27:56 -0600

    Adds favorite movies to backend

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,71 @@
-import React from 'react'
+import React from "react";
+import { Component } from "react";
+import { connect } from "react-redux";
 
-export const Card = ({ movie }) => {
-  const poster = movie.poster_path
-  const path = `https://image.tmdb.org/t/p/w185/${poster}`
+export class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ""
+    };
+  }
 
-  return(
-    <section className="card">
-      <h3>{ movie.title }</h3>
-      <img src={ path } alt={ movie.title } />
-      <button>Favorite</button>
-    </section>
-  )
+  addFavorites = async () => {
+    if (typeof this.props.user.id === "number") {
+      const response = await this.fetchFavorites();
+      console.log(response);
+    } else {
+      this.setState({
+        message: "Please login or sign up"
+      });
+    }
+  };
+
+  fetchFavorites = async () => {
+    const { movie, user } = this.props;
+    let data = {
+      movie_id: movie.id,
+      user_id: user.id,
+      title: movie.title,
+      poster_path: movie.poster_path,
+      release_date: movie.release_date,
+      vote_average: movie.vote_average,
+      overview: movie.overview,
+      isFavorite: true
+    };
+    
+    const url = "http://localhost:3000/api/users/favorites/new";
+    const addFav = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    
+    const response = await addFav.json();
+    return response;
+  };
+    
+   
+
+  render() {
+    const { movie } = this.props;
+    const poster = movie.poster_path;
+    const path = `https://image.tmdb.org/t/p/w185/${poster}`;
+    return (
+      <section className="card">
+        <h3>{movie.title}</h3>
+        <img src={path} alt={movie.title} />
+        <button onClick={this.addFavorites}>Favorite</button>
+        {this.state.message && this.state.message}
+      </section>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+    user: state.user
+});
+
+export default connect(mapStateToProps)(Card);
