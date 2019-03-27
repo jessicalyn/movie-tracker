40c18513506bed38040cfecc6a80c13c399fbe33
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-03-27 09:28:58 -0600

    Start fixing Card tests

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import  { Card } from './Card'
 import { shallow } from 'enzyme';
 
 describe('Card', ()=> {
-    describe('Login', () => {
+    describe('Card', () => {
         let wrapper; 
         beforeEach(() => {
             let mockMovie = {
@@ -16,20 +15,23 @@ describe('Card', ()=> {
                 overview: "movie.overview",
             }
             let mockUser = {
-                id: 1
+                id: 1,
+                name: "Archie",
+                favorites: [
+                  {title: "Cars", poster_path: "car.jpg"},
+                  {title: "How To Train Your Dragon", poster_path: "dragon.jpg"}
+                ]
             }
-            wrapper = shallow(<Card user={mockUser} movie={mockMovie} />)
+            wrapper = shallow(<Card user={mockUser} movie={mockMovie} hasError={jest.fn()} updateUser={jest.fn()}/>)
         })
 
         it('should match the snapshot with all data passed in', () => {
             expect(wrapper).toMatchSnapshot()
         })
 
-        it('should invoke addfavorites', () => {
-
-           wrapper.instance().fetchFavorites = jest.fn()
-            wrapper.instance().addFavorites()
-            expect(wrapper.instance().fetchFavorites).toBeCalled()
+        it('should call validateFavorites when favorite button is clicked and user is logged in', () => {
+          wrapper.instance().addFavorites()
+          expect(wrapper.instance().validateFavorites()).toBeCalled()
         })
 
         it('should add message if user id is not defined', () => {
